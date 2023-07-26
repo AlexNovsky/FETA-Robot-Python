834deffa27a40a0fe3439754589d518ef38ccd05
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2023-07-25 21:54:49 -0500

    Updated requirements, added functions to base_page, updated test and resource file

--- resources/common/pages/base_page.py
+++ resources/common/pages/base_page.py
@@ -1,7 +1,8 @@
 import random
-import time
 import yaml
+import time
 import os
+
 
 def generate_username(name: str) -> str:
     """
@@ -9,10 +10,22 @@ def generate_username(name: str) -> str:
         :param name:        String argument
         :return:            generated_name string variable
     """
-    timestamp = int(time.time())
+    timestamp = str(int(time.time()))
     generated_name = f'{name}{timestamp}'
     if generated_name == f'{name}{timestamp}':
-        return str(generated_name)
+        return str(generated_name).capitalize()
+
+def generate_phone() -> str:
+    """
+    Generates US based random 10 digits phone number
+        :return:            phone_number str variable
+    """
+    phone_number_lenght = 9
+    phone_number = '1'
+    numbers = get_whitelisted_digits()
+    for i in range(phone_number_lenght):
+        phone_number += ''.join(random.choice(str(numbers)))
+    return str(int(phone_number))
 
 def get_whitelisted_digits() -> str:
     config_folder = os.path.join(os.path.dirname(__file__), '..', 'data')
@@ -23,14 +36,33 @@ def get_whitelisted_digits() -> str:
     whitelisted_digits = data['whitelisted_characters']['digits']
     return whitelisted_digits
 
-def generate_phone() -> int:
-    """
-    Generates US based random 10 digits phone number
-        :return:            phone_number int. variable
-    """
-    phone_number_lenght = 9
-    phone_number = '1'
-    numbers = get_whitelisted_digits()
-    for i in range(phone_number_lenght):
-        phone_number += ''.join(random.choice(str(numbers)))
-    return int(phone_number)
+def generate_email():
+    config_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'demoqa', 'data')
+    config_file_path = os.path.join(config_folder, 'demoqa_data.yaml')
+    with open(config_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    timestamp = str(int(time.time()))
+    email = data['test_user']['email'].replace('{placeholder}', timestamp)
+    return email.lower()
+
+def get_gender_locator():
+    config_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'demoqa', 'data')
+    config_file_path = os.path.join(config_folder, 'demoqa_data.yaml')
+    with open(config_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    gender = str(data['test_user']['gender'])
+    if gender.capitalize() == 'Male':
+        return str("//label[contains(text(),'Male')]")
+    elif gender.capitalize() == 'Female':
+        return str("//label[contains(text(),'Female')]")
+    else:
+        return str("//label[contains(text(),'Other')]")
+
+def get_hobbies():
+    config_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'demoqa', 'data')
+    config_file_path = os.path.join(config_folder, 'demoqa_data.yaml')
+    with open(config_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    hobbies = str(data['test_user']['hobbies'])
+    # TODO defining checkbox locators based on val. hobbies
--- tests/demoqa/tc01-practice_form.robot
+++ tests/demoqa/tc01-practice_form.robot
@@ -1,8 +1,8 @@
 *** Settings ***
 Library  SeleniumLibrary
-Variables    resources/demoqa/data/demoqa_data.yaml
-Resource    resources/common/common_web.robot
-Resource    resources/demoqa/pages/practice_form.robot
+Variables   resources/demoqa/data/demoqa_data.yaml
+Resource    resources/common/common_web.resource
+Resource    resources/demoqa/pages/practice_form.resource
 
 Suite Setup  Begin Web Test
 Suite Teardown  End Web Test
@@ -12,17 +12,16 @@ ${uri}     ${base_url}/automation-practice-form
 
 *** Test Cases ***
 Verify Practice form loaded
-    Go To    ${uri}
-    practice_form.verify_happy_elements
+    Go To                                   ${uri}
+#    practice_form.verify_happy_elements
 
 Fill Practice form with generated credentials
     practice_form.generate_first_name
     practice_form.generate_last_name
-    practice_form.enter_email
+    practice_form.generate_user_email
     practice_form.generate_phone_number
-    practice_form.enter_dob
-
-    Sleep  5
+    practice_form.select_gender_radio
+    Sleep  3
 
 #Fill Practice form with defined credentials
 #    practice_form.enter_first_name
